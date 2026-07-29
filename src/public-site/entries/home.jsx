bb727e23e5736d390615010bf1aae6cdda5d7edf
jeakyungdrive01-art <jeakyungdrive01@gmail.com>
2026-07-29 23:17:51 +0900

    feat: migrate public header and footer to React

---
+++ src/public-site/entries/home.jsx
@@ -0,0 +1,18 @@
+import { flushSync } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+import PublicFooter from '../components/layout/PublicFooter.jsx';
+import PublicHeader from '../components/layout/PublicHeader.jsx';
+
+const header = document.getElementById('header');
+const footer = document.querySelector('.site-footer');
+
+if (header) {
+  const headerRoot = createRoot(header);
+  flushSync(() => headerRoot.render(<PublicHeader page="home" />));
+}
+
+if (footer) {
+  const footerRoot = createRoot(footer);
+  flushSync(() => footerRoot.render(<PublicFooter page="home" />));
+}
